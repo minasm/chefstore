7e2b3371e32fff4e1d082e156b3deb06714197f6
minasm <minas@miroglu.com>
2025-09-18 10:18:45 +0100

    chore: Controller cheks

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,9 +11,9 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $categories = Category::with('faqs')->get();
+        $categories = Category::with('faqs')->paginate(10);
         return Inertia::render('Welcome')->with('categories', $categories);
     }
 
