8b9e385398355dce1efaa0561f1a4349260f6539
minasm <minas@miroglu.com>
2025-09-21 22:04:13 +0100

    chore: replace FilamentInfoWidget with CategoriesFaqsOverview in GratasPanelProvider

--- app/Providers/Filament/GratasPanelProvider.php
+++ app/Providers/Filament/GratasPanelProvider.php
@@ -11,7 +11,7 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
+use App\Filament\Widgets\CategoriesFaqsOverview;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -41,7 +41,7 @@ class GratasPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
-                FilamentInfoWidget::class,
+                CategoriesFaqsOverview::class,
             ])
             ->middleware([
                 EncryptCookies::class,
